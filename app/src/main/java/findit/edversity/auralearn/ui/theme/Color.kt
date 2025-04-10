7632d3c6558f4ab95ba336cdaf8abd5ee3201797
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2025-04-10 15:21:00 +0700

    feat: auralearn color theme

--- app/src/main/java/findit/edversity/auralearn/ui/theme/Color.kt
+++ app/src/main/java/findit/edversity/auralearn/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package findit.edversity.auralearn.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PurplePrimary = Color(0xFF7F54FF)
+val PurpleSecondary = Color(0xFF250152)
+val CyanTertiary = Color(0xFF3BF5EC)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
